79c5b609debf384e1246967927d98b823f015580
banjak189 <banjak189@gmail.com>
2025-11-17 13:11:25 +0300

    use clean server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env. || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
